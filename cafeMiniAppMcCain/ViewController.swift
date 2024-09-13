44e5fdaf0feac7fc46f6278e5c68ddb75f0bea56
seanmccain08 <seanmccain327@gmail.com>
2024-09-13 08:01:29 -0500

    Initial Commit

--- cafeMiniAppMcCain/ViewController.swift
+++ cafeMiniAppMcCain/ViewController.swift
@@ -7,13 +7,66 @@
 
 import UIKit
 
+var foods = ["Apple","Banana","Chicken","Pizza","Ham"]
+var prices = [0.75,0.55,7.55,14.99,9.99]
+var shoppingCart: [String: Double] = [:]
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var errorLabel: UILabel!
+    @IBOutlet weak var quantityField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        var text = ""
+        for i in 0..<foods.count{
+            
+            text += foods[i]
+            text += "  -  "
+            text += "$\(prices[i])\n"
+            
+        }
+        textView.text = text
+        
     }
 
-
+    @IBAction func addButton(_ sender: Any) {
+        
+        var temp = false
+        for item in foods{
+            
+            if textField.text == item {
+                
+                temp = true
+                errorLabel.isHidden = true
+                
+            }
+            
+        }
+        if(temp == false){
+            
+            errorLabel.isHidden = false
+            errorLabel.text = "Error: Specified item does not exist"
+            
+        }
+        
+        
+    }
+    
+    @IBAction func AdminButton(_ sender: Any) {
+        
+        
+        
+    }
+    
+    @IBAction func cartButton(_ sender: Any) {
+        
+        
+        
+    }
+    
 }
 
---
+++ cafeMiniAppMcCain/ViewController1.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController1.swift
+//  cafeMiniAppMcCain
+//
+//  Created by SEAN MCCAIN on 9/13/24.
+//
+
+import UIKit
+
+class ViewController1: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    }
+    
+    
+    
+}
